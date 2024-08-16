651eeed20a787eb5b9f6edd3a8b24d37c5b7d715
yoxiaoxiao <xiaoxiaofu2866@gmail.com>
2024-08-16 16:29:50 -0700

    finished aa01-array-splice

--- problems/01-remove-last-vowel.js
+++ problems/01-remove-last-vowel.js
@@ -5,11 +5,23 @@ Vowels are the letters "a", "e", "i", "o", "u".
 */
 
 // Your code here 
+function removeLastVowel(word) {
+    let vowels = ["a", "e", "i", "o", "u"];
+    let characters = word.split("");
 
-// console.log(removeLastVowel('bootcamp')); // 'bootcmp'
-// console.log(removeLastVowel('better')); // 'bettr'
-// console.log(removeLastVowel('graph')); // 'grph'
-// console.log(removeLastVowel('thy')); // 'thy'
+    for (let i = characters.length - 1; i >= 0; i--) {
+        if (vowels.includes(characters[i])) {
+            characters.splice(i, 1);
+            break;
+        }
+    }
+    return characters.join("");
+}
+
+console.log(removeLastVowel('bootcamp')); // 'bootcmp'
+console.log(removeLastVowel('better')); // 'bettr'
+console.log(removeLastVowel('graph')); // 'grph'
+console.log(removeLastVowel('thy')); // 'thy'
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = removeLastVowel;
